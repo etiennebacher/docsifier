37824cca23f6ee559c5fc082a0fd034dd4c591b8
etiennebacher <etienne.bacher@orange.fr>
2020-11-27 19:51:55 +0100

    node not necessary

--- R/dep_available.R
+++ R/dep_available.R
@@ -2,29 +2,12 @@
 # https://colinfay.me/node-r-package/
 
 #' Check if Node and npm are installed
-#' @name node_npm
+#'
 #' @export
 npm_available <- function(){
   test <- suppressWarnings(
     system(
       "npm -v",
-      ignore.stdout = TRUE,
-      ignore.stderr = TRUE
-    )
-  )
-  if (test != 0) {
-    return(FALSE)
-  } else {
-    return(TRUE)
-  }
-}
-
-#' @rdname node_npm
-#' @export
-node_available <- function(){
-  test <- suppressWarnings(
-    system(
-      "node -v",
       ignore.stdout = TRUE,
       ignore.stderr = TRUE
     )
