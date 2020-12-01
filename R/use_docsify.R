63b2d305a5aca75779a636a00c441e0b785774ed
etiennebacher <etienne.bacher@orange.fr>
2020-12-01 22:49:12 +0100

    add function to create a "Reference" page if the user wants to, whether when using use_docsify or after.

--- R/use_docsify.R
+++ R/use_docsify.R
@@ -1,15 +1,23 @@
 #' Create the structure to use docsify in an R package
 #'
-#' @param open Boolean indicating whether to open "index.html" and "homepage.md" following their creation.
+#' @param open Boolean indicating whether to open the HTML and Markdown files created. Defaukt is TRUE.
+#' @param add_reference Boolean indicating whether to add a Markdown file containing function references, i.e the list of functions (and their title and arguments) exported by the package. Default is TRUE.
+#' @param include_internal Boolean indicating if you want to include the documentation of internal (i.e non-exported functions). Default is TRUE. See Details.
 #'
-#' @details This function creates the folder "/docs" if it doesn"t already exist, and "index.html" to create the documentation with docsify. The structure of "index.html" is automatically created and you can complete it with your custom info.
-#'
+#' @details This function creates the folder "/docs" (if it doesn't already exist) and "index.html" to create the documentation with docsify.js. The structure of "index.html" is automatically created and you can complete it with your custom info.
 #' You can use `add_md()` to add Markdown files in "/docs" to populate the documentation.
+#'
+#' You can add a page containing the list of functions that your package provide, and their documentation. Internal functions (i.e functions that are not exported by the package) are included by default. If you don't want to include them, add "@@keywords internal" in the roxygen block of the function concerned, and use `include_internal = FALSE`.
+#'
 #'
 #' @importFrom utils file.edit
 #' @export
 
-use_docsify <- function(open = TRUE) {
+use_docsify <- function(
+  open = TRUE,
+  add_reference = TRUE,
+  include_internal = TRUE
+) {
 
   ### Only works in a package
 
@@ -67,31 +75,51 @@ use_docsify <- function(open = TRUE) {
     system.file("templates/homepage-template.md", package = "docsifier"),
     "docs/homepage.md"
   )
-  fs::file_copy(
-    system.file("templates/sidebar-template.md", package = "docsifier"),
-    "docs/_sidebar.md"
-  )
+
+  if (isTRUE(add_reference)) {
+    fs::file_copy(
+      system.file("templates/sidebar-template2.md", package = "docsifier"),
+      "docs/_sidebar.md"
+    )
+  } else {
+    fs::file_copy(
+      system.file("templates/sidebar-template.md", package = "docsifier"),
+      "docs/_sidebar.md"
+    )
+  }
+
   fs::file_copy(
     system.file("templates/howto-template.md", package = "docsifier"),
     "docs/howto.md"
   )
+
   if (isTRUE(open)) {
     if (rstudioapi::isAvailable()) {
       invisible(lapply(
-        c("docs/_sidebar.md", "docs/homepage.md", "docs/howto.md", "docs/index.html"),
+        c("docs/_sidebar.md", "docs/homepage.md",
+          "docs/howto.md", "docs/index.html"),
         function(x) {
           rstudioapi::navigateToFile(x)
           return()
         }
       ))
     } else {
-      file.edit(c("docs/_sidebar.md", "docs/homepage.md", "docs/howto.md", "docs/index.html"))
+      file.edit(c("docs/_sidebar.md", "docs/homepage.md",
+                  "docs/howto.md", "docs/index.html"))
     }
   }
   cli::cat_bullet(
     bullet_col = "green", bullet = "tick",
     'Files "homepage.md", "_sidebar.md", and "howto.md" have been created.'
   )
+
+
+  ### Add a page with function references if user wants
+
+  if (isTRUE(add_reference)) {
+    add_function_references(include_internal)
+  }
+
 
 
 }
---
+++ inst/templates/sidebar-template2.md
@@ -0,0 +1,5 @@
+<!-- homepage.md is defined as the first page in index.html  --> 
+
+* [Home](/)
+* [How to use](howto.md)
+* [Reference](func_reference.md)
