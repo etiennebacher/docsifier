11a023cbb69d7eba066b6ced5bd74394bc2dc924
etiennebacher <you@example.com>
2021-01-31 19:46:52 +0100

    gather add_css, add_rmd and add_md functions together

--- R/add.R
+++ R/add.R
@@ -96,3 +96,68 @@ add_rmd <- function(name, open = TRUE) {
   )
 
 }
+
+
+#' Add a CSS file to your documentation
+#'
+#' @param open Open or not the file created. Default is TRUE.
+#' @param name Name to give to the CSS file you want to create. If `NULL`, the file will be named `custom.css`
+#'
+#'
+#' @export
+#'
+#' @return Creates a CSS file in "docs/_assets/css"
+#'
+#' @examples
+#' \dontrun{
+#'
+#' library(docsifier)
+#'
+#' # Create a test folder and a test package for the example
+#'
+#' test_folder <- tempdir()
+#' setwd(test_folder)
+#' devtools::create("dummy")
+#' setwd("dummy")
+#'
+#' # Generate the minimal documentation for docsify.js
+#'
+#' init_docsify()
+#'
+#' # Create "custom.css"  in "/docs/_assets/css"
+#'
+#' add_css()
+#' }
+
+add_css <- function(name = NULL, open = TRUE) {
+
+  if (missing(name)) name <- "custom"
+  if (name == "") name <- "custom"
+  if (!file.exists("docs/_assets/css")) {
+    fs::dir_create("docs/_assets/css")
+  }
+
+  file_name <- paste0(name, ".css")
+  file_path <- paste0("docs/_assets/css/", file_name)
+
+  if (file.exists(paste0("docs/_assets/css/", file_name)))
+    stop(paste0("File ", file_name, " already exists."))
+
+  fs::file_create(file_path)
+
+  if (isTRUE(open)) {
+    if (rstudioapi::isAvailable()) {
+      rstudioapi::navigateToFile(file_path)
+    } else {
+      utils::file.edit(file_path)
+    }
+  }
+
+  message_validate(
+    paste0('File "docs/_assets/css/', file_name, '" has been created.')
+  )
+  message_info(
+    paste0("Don't forget to add <link rel='stylesheet' href='/_assets/css/", file_name, "'/> in the head of 'index.html'")
+  )
+}
+
