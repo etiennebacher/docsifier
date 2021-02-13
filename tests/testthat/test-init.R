6712afce547592cc55a7193e1999dd38f1b9e122
etiennebacher <you@example.com>
2021-02-13 22:10:06 +0100

    update the tests for init

--- tests/testthat/test-init.R
+++ tests/testthat/test-init.R
@@ -67,13 +67,13 @@ test_that("init_docsify adds 'docs' in .Rbuildignore if not already in", {
 
 test_that("init_docsify has an error if creation of reference is asked but no 'man' folder", {
   create_local_package()
-  expect_message(init_docsify(open = FALSE, add_reference = TRUE))
+  expect_message(init_docsify(open = FALSE))
 })
 
 test_that("init_docsify creates a file for references if asked and if 'man' exists", {
   create_local_package()
   dir_create("man")
-  init_docsify(open = FALSE, add_reference = TRUE)
+  init_docsify(open = FALSE)
   expect_proj_file("docs/reference.md")
 })
 
@@ -110,7 +110,7 @@ test_that("if readme exists, its content is put on homepage", {
   fs::file_create("README.md")
   cat("This is the content from the README file", file = "README.md", append = TRUE)
 
-  init_docsify(open = FALSE, add_reference = FALSE)
+  init_docsify(open = FALSE)
 
   x <- suppressWarnings(readLines("README.md"))
   y <- suppressWarnings(readLines("docs/homepage.md"))
@@ -128,7 +128,7 @@ test_that("if news.md exists, it is included in the webpage", {
   fs::file_create("NEWS.md")
   cat("This is the content from the NEWS.md file", file = "NEWS.md", append = TRUE)
 
-  init_docsify(open = FALSE, add_reference = FALSE)
+  init_docsify(open = FALSE)
 
   x <- suppressWarnings(readLines("NEWS.md"))
   y <- suppressWarnings(readLines("docs/NEWS.md"))
@@ -141,5 +141,76 @@ test_that("if news.md exists, it is included in the webpage", {
     fixed = TRUE
   )
   expect_true(TRUE %in% news_in_sidebar)
+
+})
+
+
+### Add License
+
+test_that("if license exists, it is included in the webpage", {
+  create_local_package()
+
+  fs::file_create("LICENSE.md")
+  cat("This is the content from the LICENSE file", file = "LICENSE.md", append = TRUE)
+
+  init_docsify(open = FALSE)
+
+  x <- suppressWarnings(readLines("LICENSE.md"))
+  y <- suppressWarnings(readLines("docs/LICENSE.md"))
+  expect_identical(x, y)
+
+  sidebar_file <- suppressWarnings(readLines("docs/_sidebar.md"))
+  license_in_sidebar <- grepl(
+    "* [License](LICENSE.md)",
+    sidebar_file,
+    fixed = TRUE
+  )
+  expect_true(TRUE %in% license_in_sidebar)
+
+})
+
+test_that("if license exists, it is included in the webpage", {
+  create_local_package()
+
+  fs::file_create("LICENCE.md")
+  cat("This is the content from the LICENCE file", file = "LICENCE.md", append = TRUE)
+
+  init_docsify(open = FALSE)
+
+  x <- suppressWarnings(readLines("LICENCE.md"))
+  y <- suppressWarnings(readLines("docs/LICENCE.md"))
+  expect_identical(x, y)
+
+  sidebar_file <- suppressWarnings(readLines("docs/_sidebar.md"))
+  licence_in_sidebar <- grepl(
+    "* [Licence](LICENCE.md)",
+    sidebar_file,
+    fixed = TRUE
+  )
+  expect_true(TRUE %in% licence_in_sidebar)
+
+})
+
+### Add CoC
+
+test_that("if CoC exists, it is included in the webpage", {
+  create_local_package()
+
+  fs::file_create("CODE_OF_CONDUCT.md")
+  cat("This is the content from the CODE_OF_CONDUCT file", file = "CODE_OF_CONDUCT.md", append = TRUE)
+
+  init_docsify(open = FALSE)
+
+  x <- suppressWarnings(readLines("CODE_OF_CONDUCT.md"))
+  y <- suppressWarnings(readLines("docs/CODE_OF_CONDUCT.md"))
+  expect_identical(x, y)
+
+  sidebar_file <- suppressWarnings(readLines("docs/_sidebar.md"))
+  license_in_sidebar <- grepl(
+    "* [Code of Conduct](CODE_OF_CONDUCT.md)",
+    sidebar_file,
+    fixed = TRUE
+  )
+  expect_true(TRUE %in% license_in_sidebar)
 
 })
